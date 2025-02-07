ca9fab960ac297f11bc6e0ae20f73b7bf554d790
Jordan Higgins <jordanhiggins@usf.edu>
2025-02-07 17:22:00 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -7,3 +7,17 @@ let customer = {
 
 console.log(`Name: ${customer.name}, Age: ${customer.age}, Email: ${customer.email}`);
 // Declared the object customer with the name, age, and email properties. Then logged everything to the console.
+
+// Task 2: Order Details
+let order = {
+    orderID: 10001,
+    totalAmount: 100,
+    status: "Pending",
+
+    displayOrder() {
+        console.log(`Order ID: ${this.orderID}, Total Amount: ${this.totalAmount}, Status: ${this.status}`);
+    }
+
+};
+order.displayOrder();
+// Declared order as an object with three values. Then assigned the values to displayOrder and had all the values logged to the console.
