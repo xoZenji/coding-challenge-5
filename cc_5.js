1c5571779ec49bb97e2697675660c6742d9069c0
Jordan Higgins <jordanhiggins@usf.edu>
2025-02-06 19:10:51 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,9 @@
+// Task 1: Customer Profile
+let customer = {
+    name: "Michael Cera",
+    age: 36,
+    email: "mcera@gmail.com"
+};
+
+console.log(`Name: ${customer.name}, Age: ${customer.age}, Email: ${customer.email}`);
+// Declared the object customer with the name, age, and email properties. Then logged everything to the console.
